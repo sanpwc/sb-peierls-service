8c2282291c0ff97121a1b6da52826ca9b0450f96
Alexander Lapin <alexander.lapin@autodesk.com>
2018-02-11 22:45:12 +0300

    initial commit of very simple spring boot application

---
+++ src/main/java/com/san/springboot/Application.java
@@ -0,0 +1,20 @@
+package com.san.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sun.misc.Contended;
+
+@Controller
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String showHelloWorld() {
+        return "hello-world";
+    }
+}
